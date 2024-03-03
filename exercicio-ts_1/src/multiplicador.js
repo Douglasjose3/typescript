c3c3432653e31a751b4a400fcc767c630331d86d
Douglasjose3 <douglasjose3@gmail.com>
2024-03-02 20:34:47 -0800

    exercício pronto, tentativa 1

---
+++ exercicio-ts_1/src/multiplicador.js
@@ -0,0 +1,8 @@
+"use strict";
+function multiplicar(numeroA, numeroB) {
+    return numeroA * numeroB;
+}
+const resultado = multiplicar(39, 82);
+console.log(resultado);
+const saudacao = ['Douglas'];
+console.log(`Olá ${saudacao}`);
